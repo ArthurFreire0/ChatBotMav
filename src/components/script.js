09db7b43eb3d001c03968c702fd0e3fdb7aea330
kaiquemussa <kaiquemussa66@gmail.com>
2025-06-12 21:48:57 -0300

    update: remove table locations and use map link

--- src/components/script.js
+++ src/components/script.js
@@ -9,23 +9,75 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const respostas = {
     1: "O Movimento Água é Vida, também conhecido como MAV, é uma Organização da Sociedade Civil (OSC), sem fins lucrativos (OSFL), que foi fundada no dia 21 de maio de 1997 Nossa missão é cuidar da água, da saúde e do meio ambiente, por meio de atividades representativas, educacionais e operacionais de reciclagem junto à comunidade.",
-    2: `<strong>Pontos de Coleta da MAV:</strong><br><table border='1' style="width: 100%; font-size: 12px;">
-          <tr><td>Paróquia Santo Antônio</td><td>Av. Pres. Dutra - Capuchinhos</td><td>(75) 3625-1910</td></tr>
-          <tr><td>Paróquia Cristo Redentor</td><td>Rua Lopes Rodrigues, 10 - Jomafa</td><td>(75) 3223-4423</td></tr>
-          <tr><td>EMBASA - Fundo do Cuca</td><td>R. Des. Felinto Bastos - Centro</td><td>(75) 9112-4745</td></tr>
-          <tr><td> EMBASA -Fundo do Cuca </td><td>R. Des. Felinto Bastos- Centro</td><td(75) 9112-4745</td></tr>
-          <tr><td>Paróquia Nossa Senhora do Perpétuo Socorro</td><td>Travessa Macaria Barreto, 35 - Tomba</td><td>(75) 3622-5689</td></tr>
-          <tr><td>Secretaria de Meio Ambiente - SEMAM</td><td>R. Leolinda Bacelar Lima, 224 - Centro</td><td>(75) 3322-9319</td></tr>
-          <tr><td>Secretaria de Serviços Públicos - SESP</td><td>R. dos Tupinambás, 275 - São João</td><td>(75) 3602-8100</td></tr>
-          <tr><td>Comunidade Cristo Operário - Aviário</td><td>R. Principal - Aviário</td><td>(75) 8159-6027</td></tr>
-          <tr><td>Centro de Referência de Assistência Social - CRAS</td><td>Todos da Cidade</td><td>(75) 8140-4546</td></tr>
-          <tr><td>Unidade Básica de Saúde - Caseb I</td><td>R. Japão S/N</td><td>(75) 9263-0940</td></tr>
-          <tr><td>Universidade Estadual de Feira de Santana - UEFS</td><td>Av. Transnordestina, s/n - Novo Horizonte</td><td>(75) 3161-8000</td></tr>
-          <tr><td>Igreja Batista Central - Centro</td><td>Av. Getúlio Vargas, 1990 - Ponto Central</td><td>(75) 3612-6500</td></tr>
-          <tr><td>Feira Produtiva - Centro de Abastecimento</td><td>Centro de Abastecimento</td><td>(75) 9118-8465</td></tr>
-          <tr><td>CSU - Centro Social Urbano - Cidade Nova</td><td>R. Tostão - Cidade Nova</td><td>(75) 9143-9870</td></tr>  
-        </table>`,
-    3: `Você pode ajudar o Grupo MAV por meio de doações que podem ser feitas pelo <a href="https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=JKCRL4DF4B26N&source=url" target="_blank" rel="noopener noreferrer" style="color: #003d66">PAYPAL`,
+    2: `<h2>Pontos de Coleta da MAV</h2>
+    <ul>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=Av.+Pres.+Dutra+-+Capuchinhos" target="_blank"><strong>Paróquia Santo Antônio</strong></a><br>
+        Endereço: Av. Pres. Dutra - Capuchinhos.<br>
+        Telefone: (75) 3625-1910.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=Rua+Lopes+Rodrigues,+10+-+Jomafa" target="_blank"><strong>Paróquia Cristo Redentor</strong></a><br>
+        Endereço: Rua Lopes Rodrigues, 10 - Jomafa.<br>
+        Telefone: (75) 3223-4423.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=R.+Des.+Felinto+Bastos+-+Centro" target="_blank"><strong>EMBASA - Fundo do Cuca</strong></a><br>
+        Endereço: R. Des. Felinto Bastos - Centro.<br>
+        Telefone: (75) 9112-4745.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=Travessa+Macaria+Barreto,+35+-+Tomba" target="_blank"><strong>Paróquia Nossa Senhora do Perpétuo Socorro</strong></a><br>
+        Endereço: Travessa Macaria Barreto, 35 - Tomba.<br>
+        Telefone: (75) 3622-5689.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=R.+Leolinda+Bacelar+Lima,+224+-+Centro" target="_blank"><strong>Secretaria de Meio Ambiente - SEMAM</strong></a><br>
+        Endereço: R. Leolinda Bacelar Lima, 224 - Centro.<br>
+        Telefone: (75) 3322-9319.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=R.+dos+Tupinambás,+275+-+São+João" target="_blank"><strong>Secretaria de Serviços Públicos - SESP</strong></a><br>
+        Endereço: R. dos Tupinambás, 275 - São João.<br>
+        Telefone: (75) 3602-8100.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=R.+Principal+-+Aviário" target="_blank"><strong>Comunidade Cristo Operário - Aviário</strong></a><br>
+        Endereço: R. Principal - Aviário.<br>
+        Telefone: (75) 8159-6027.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=Centro+de+Referência+de+Assistência+Social+-+Feira+de+Santana" target="_blank"><strong>Centro de Referência de Assistência Social - CRAS</strong></a><br>
+        Endereço: Todos da Cidade.<br>
+        Telefone: (75) 8140-4546.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=R.+Japão+S/N" target="_blank"><strong>Unidade Básica de Saúde - Caseb I</strong></a><br>
+        Endereço: R. Japão S/N.<br>
+        Telefone: (75) 9263-0940.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=Av.+Transnordestina,+s/n+-+Novo+Horizonte" target="_blank"><strong>Universidade Estadual de Feira de Santana - UEFS</strong></a><br>
+        Endereço: Av. Transnordestina, s/n - Novo Horizonte.<br>
+        Telefone: (75) 3161-8000.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=Av.+Getúlio+Vargas,+1990+-+Ponto+Central" target="_blank"><strong>Igreja Batista Central - Centro</strong></a><br>
+        Endereço: Av. Getúlio Vargas, 1990 - Ponto Central.<br>
+        Telefone: (75) 3612-6500.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=Centro+de+Abastecimento+Feira+de+Santana" target="_blank"><strong>Feira Produtiva - Centro de Abastecimento</strong></a><br>
+        Endereço: Centro de Abastecimento.<br>
+        Telefone: (75) 9118-8465.
+      </li>
+      <li>
+        <a href="https://www.google.com/maps/search/?q=R.+Tostão+-+Cidade+Nova" target="_blank"><strong>CSU - Centro Social Urbano - Cidade Nova</strong></a><br>
+        Endereço: R. Tostão - Cidade Nova.<br>
+        Telefone: (75) 9143-9870.
+      </li>
+    </ul>`,
+    3: `Você pode ajudar o Grupo MAV por meio de doações que podem ser feitas pelo <a href="https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=JKCRL4DF4B26N&source=url" target="_blank"  rel="noopener noreferrer" style="color: #003d66"><i class="fa-brands fa-paypal"></i> PAYPAL`,
     4: `PROJETO: ÁGUA VIVA Reaproveitamento de óleo vegetal e gorduras residuais<br>
         PROJETO: ÁRVORE VIVA<br>
         PROJETO: PEDAGÓGICO<br>
